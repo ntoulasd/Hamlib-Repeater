a779da1b8aeaf16663e98c90f45565d671ffdb7a
Dimitrios Ntoulas <ntoulasd@yahoo.gr>
2016-04-18 01:38:55 +0300

    Typo

--- repeater.sh
+++ repeater.sh
@@ -7,7 +7,7 @@ signal=`rigctl  -m 2 -r localhost:4532 l STRENGTH`
 if [ $signal -gt -45 ]
 then
 echo "Signal detected "$signal
-#Transmeter Config ip and port
+#Transmitter Config ip and port
 rigctl -m 2 -r localhost:4533 T 1
 else 
 rigctl -m 2 -r localhost:4533 T 0
